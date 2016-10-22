4c4475f5c06cb78aa6d7440ddf56eafafc643dd7
Alex Fong <actfong@gmail.com>
2016-10-22 11:06:05 +0200

    Syntax changes to specs

--- spec/vat_spec.rb
+++ spec/vat_spec.rb
@@ -8,24 +8,21 @@ describe Apilayer::Vat do
   end
 
   describe :connection do
+    subject { Apilayer::Vat.connection }
+
     context "vat_layer access_key has been set" do
       it "returns a connection with correct attributes" do
-        conn = Apilayer::Vat.connection
-
-        expect(conn).to be_a Faraday::Connection
-        expect(conn.url_prefix.host).to match "apilayer.net"
-        expect(conn.params["access_key"]).to eq "vat_layer_key123"
+        expect(subject).to be_a Faraday::Connection
+        expect(subject.url_prefix.host).to match "apilayer.net"
+        expect(subject.params["access_key"]).to eq "vat_layer_key123"
       end
     end
 
-    context "non-secure connection" do
-      it "returns a connection to http protocol" do
-        conn = Apilayer::Vat.connection
-        expect(conn.url_prefix).to be_a URI::HTTP
-      end
+    context "configured as non-secure connection" do
+      specify{ expect(subject.url_prefix).to be_a URI::HTTP }
     end
 
-    context "secure connection" do
+    context "configured as secure connection" do
       before do
         Apilayer::Vat.configure do |configs|
           configs.https = true
@@ -37,19 +34,21 @@ describe Apilayer::Vat do
         end
       end
 
-      it "returns a connection to https protocol" do
-        conn = Apilayer::Vat.connection
-        expect(conn.url_prefix).to be_a URI::HTTPS
-      end
+      specify{ expect(subject.url_prefix).to be_a URI::HTTPS }
     end
   end
 
   describe :validate do
+    subject { Apilayer::Vat.validate(vat_number) }
+    let(:vat_number) { "LU26375245" }
+
     it "returns a Hash with VAT data" do
       VCR.use_cassette("vat/validation") do
-        api_resp = Apilayer::Vat.validate("LU26375245")
-        expect(api_resp["query"]).to eq "LU26375245"
-        expect(api_resp["company_name"]).to eq "AMAZON EUROPE CORE S.A R.L."
+
+        expect(subject).to include(
+          "query" =>  "LU26375245",
+          "company_name" => "AMAZON EUROPE CORE S.A R.L."
+        )
       end
     end
 
@@ -58,18 +57,24 @@ describe Apilayer::Vat do
         expect(Apilayer::Vat).to receive(:get_and_parse).with(
           "validate", {:vat_number => "LU26375245"}
         )
-        Apilayer::Vat.validate("LU26375245")
-      end      
+        subject
+      end
     end
   end
 
   describe :rate do
+    subject { Apilayer::Vat.rate(criteria, value) }
+
     context "country_code provided as criteria" do
+      let(:criteria) { :country_code }
+      let(:value) { "NL" }
+
       it "returns a Hash with VAT rate for the specified country" do
         VCR.use_cassette("vat/rate_by_country_code") do
-          api_resp = Apilayer::Vat.rate(:country_code, "NL")
-          expect(api_resp['standard_rate']).to be_a Numeric
-          expect(api_resp['reduced_rates']).to be_a Hash
+          expect(subject).to include(
+            'standard_rate' => kind_of(Numeric),
+            'reduced_rates' => kind_of(Hash)
+          )
         end
       end
 
@@ -78,17 +83,21 @@ describe Apilayer::Vat do
           expect(Apilayer::Vat).to receive(:get_and_parse).with(
             "rate", {:country_code => "NL"}
           )
-          Apilayer::Vat.rate(:country_code, "NL")
-        end      
-      end      
+          subject
+        end
+      end
     end
 
     context "ip_address provided as criteria" do
+      let(:criteria) { :ip_address }
+      let(:value) { "176.249.153.36" }
+
       it "returns a Hash with VAT rate for the specified country" do
         VCR.use_cassette("vat/rate_by_ip_address") do
-          api_resp = Apilayer::Vat.rate(:ip_address, "176.249.153.36")
-          expect(api_resp['standard_rate']).to be_a Numeric
-          expect(api_resp['reduced_rates']).to be_a Hash
+          expect(subject).to include(
+            'standard_rate' => kind_of(Numeric),
+            'reduced_rates' => kind_of(Hash)
+          )
         end
       end
 
@@ -97,32 +106,38 @@ describe Apilayer::Vat do
           expect(Apilayer::Vat).to receive(:get_and_parse).with(
             "rate", {:ip_address => "176.249.153.36"}
           )
-          Apilayer::Vat.rate(:ip_address, "176.249.153.36")
+          subject
         end
       end
-    end    
+    end
   end
 
   describe :rate_list do
+    subject { Apilayer::Vat.rate_list }
     it "returns VAT rates for all 28 EU countries" do
       VCR.use_cassette("vat/rate_list") do
-        api_resp = Apilayer::Vat.rate_list
-        expect(api_resp["rates"].count).to eq 28
+        expect(subject["rates"].count).to eq 28
       end
     end
 
     it "invokes get_and_parse" do
       VCR.use_cassette("vat/rate_list") do
         expect(Apilayer::Vat).to receive(:get_and_parse).with("rate_list")
-        Apilayer::Vat.rate_list
+        subject
       end
-    end  
+    end
   end
 
   describe :price do
+    subject { Apilayer::Vat.price(price, criteria, criteria_value) }
+    let(:price) { 100 }
+
     context "invalid criteria provided" do
+      let(:criteria) { :foobar }
+      let(:criteria_value) { "Some Value" }
+
       it 'raises an error' do
-        expect{Apilayer::Vat.price(100, :foobar, "Some Value")}.to raise_error(
+        expect{ subject }.to raise_error(
           Apilayer::Error,
           "You must provide either :country_code or :ip_address"
         )
@@ -130,13 +145,16 @@ describe Apilayer::Vat do
     end
 
     context "calculation based on country_code" do
+      let(:criteria) { :country_code }
+      let(:criteria_value) { "NL" }
       it 'returns price incl/excl VAT and country_code' do
         VCR.use_cassette("vat/price_by_country_code") do
-          api_resp = Apilayer::Vat.price(100, :country_code, "NL")
-          expect(api_resp["country_name"]).to eq "Netherlands"
-          expect(api_resp["price_excl_vat"]).to eq 100
-          expect(api_resp["price_incl_vat"]).to eq 121
-          expect(api_resp["vat_rate"]).to eq 21
+          expect(subject).to include(
+            "country_name" => "Netherlands",
+            "price_excl_vat" => 100,
+            "price_incl_vat" => 121,
+            "vat_rate" => 21
+          )
         end
       end
 
@@ -146,19 +164,22 @@ describe Apilayer::Vat do
             "price",
             hash_including(:amount => 100, :country_code => "NL")
           )
-          Apilayer::Vat.price(100, :country_code, "NL")
+          subject
         end
       end
     end
 
     context "calculation based on ip_address" do
+      let(:criteria) { :ip_address }
+      let(:criteria_value) { "176.249.153.36" }
       it "returns price incl/excl VAT and ip_address" do
         VCR.use_cassette("vat/price_by_ip_address") do
-          api_resp = Apilayer::Vat.price(100, :ip_address, "176.249.153.36")
-          expect(api_resp["country_name"]).to eq "United Kingdom"
-          expect(api_resp["price_excl_vat"]).to eq 100
-          expect(api_resp["price_incl_vat"]).to eq 120
-          expect(api_resp["vat_rate"]).to eq 20   
+          expect(subject).to include(
+            "country_name" => "United Kingdom",
+            "price_excl_vat" => 100,
+            "price_incl_vat" => 120,
+            "vat_rate" => 20
+          )
         end
       end
 
@@ -168,7 +189,7 @@ describe Apilayer::Vat do
             "price",
             hash_including(:amount => 100, :ip_address => "176.249.153.36")
           )
-          Apilayer::Vat.price(100, :ip_address, "176.249.153.36")
+          subject
         end
       end
 
